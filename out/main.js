c175576cc40b259d0ee0f8c69fc5947f66ef7618
613S018\sse <690829795@qq.com>
2017-02-15 16:53:16 +0800

    基本框架

--- out/main.js
+++ out/main.js
@@ -1,5 +1,12 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 var DrawObject = (function () {
-    function DrawObject() {
+    function DrawObject(x, y) {
+        this.x = x;
+        this.y = y;
     }
     DrawObject.prototype.draw = function () {
         this.context = Stage.getInstance().getContext();
@@ -8,14 +15,89 @@ var DrawObject = (function () {
 }());
 var Stage = (function () {
     function Stage() {
+        this.drawList = [];
     }
+    Stage.prototype.addchild = function (drawable) {
+        this.drawList.push(drawable);
+    };
+    Stage.prototype.draw = function () {
+        this.drawList.forEach(function (value) { value.draw(); });
+    };
     Stage.getInstance = function () {
-        return 0;
+        if (Stage.instance)
+            return Stage.instance;
+        else
+            Stage.instance = new Stage();
     };
-    Stage.prototype.getContext = function (context) {
+    Stage.prototype.setContext = function (context) {
+        console.log("set context");
+        this.context = context;
     };
+    Stage.prototype.getContext = function () {
+        if (this.context)
+            return this.context;
+        console.error("please cset 2D context");
+    };
+    Stage.instance = new Stage();
     return Stage;
 }());
+var Rectangle = (function (_super) {
+    __extends(Rectangle, _super);
+    function Rectangle(x, y, width, height) {
+        _super.call(this, x, y);
+        this.width = width;
+        this.height = height;
+    }
+    Rectangle.prototype.draw = function () {
+        _super.prototype.draw.call(this);
+        this.context.fillRect(this.x, this.y, this.width, this.height);
+    };
+    return Rectangle;
+}(DrawObject));
+var ImageField = (function (_super) {
+    __extends(ImageField, _super);
+    function ImageField(x, y, img, width, height) {
+        _super.call(this, x, y);
+        this.image = new Image();
+        this.image.src = img;
+    }
+    ImageField.prototype.draw = function () {
+        var _this = this;
+        _super.prototype.draw.call(this);
+        this.image.onload = function () {
+            _this.context.drawImage(_this.image, _this.x, _this.y);
+        };
+    };
+    return ImageField;
+}(DrawObject));
+var TextField = (function (_super) {
+    __extends(TextField, _super);
+    function TextField(x, y, str) {
+        _super.call(this, x, y);
+        this.str = str;
+    }
+    TextField.prototype.draw = function () {
+        _super.prototype.draw.call(this);
+        this.context.fillText(this.str, this.x, this.y);
+    };
+    return TextField;
+}(DrawObject));
+var DrawObjectContainer = (function (_super) {
+    __extends(DrawObjectContainer, _super);
+    function DrawObjectContainer() {
+        _super.apply(this, arguments);
+        this.drawList = [];
+    }
+    DrawObjectContainer.prototype.addChild = function (drawable) {
+        this.drawList.push(drawable);
+    };
+    DrawObjectContainer.prototype.draw = function () {
+        this.drawList.forEach(function (value) {
+            value.draw();
+        });
+    };
+    return DrawObjectContainer;
+}(DrawObject));
 window.onload = function () {
     alert(222);
 };
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,128 @@
-interface Drawable{
+interface objectDrawable{
     context:CanvasRenderingContext2D;
     draw();
 }
-class DrawObject implements Drawable{
+
+
+class DrawObject implements objectDrawable{
     x:number;
     y:number;
     context:CanvasRenderingContext2D;
+    
     draw(){
         this.context=Stage.getInstance().getContext();
     }
-}
-class Stage{
-    static getInstance():Stage{
-        return 0;
+    constructor(x:number,y:number){
+        this.x=x;
+        this.y=y;
     }
-    getContext(context:CanvasRenderingContext2D){
+}
+
+
+
+
+
+class Stage{
+    private static instance=new Stage();
+    private context:CanvasRenderingContext2D;
+    drawList:objectDrawable[]=[];
+
+    addchild(drawable:objectDrawable){
+        this.drawList.push(drawable);
+    }
+
+
+
+    draw(){
+        this.drawList.forEach((value)=>{value.draw()});
+    }
+
+
+    static getInstance():Stage{
+
+       if(Stage.instance)
+           return Stage.instance;
+        
+
+       else
+       Stage.instance=new Stage();
+    }
+
+
+    setContext(context:CanvasRenderingContext2D){
+        console.log("set context");
+        this.context=context;
+
+    }
+    getContext():CanvasRenderingContext2D{
+        if(this.context)
+        return this.context;
+        console.error("please cset 2D context");
+    }
+}
+
+class Rectangle extends DrawObject {
+    width: number;
+    height: number;
+    constructor(x: number, y: number, width: number, height: number) {
+        super(x, y);
+        this.width = width;
+        this.height = height;
+    }
+    draw() {
+        super.draw();
+        this.context.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+
+class ImageField extends DrawObject {
+    image:HTMLImageElement;
+    constructor(x: number, y: number,img:string,width?:number, height?:number) {
+        super(x, y);
+        this.image = new Image();
+        this.image.src = img;
+    }
+    draw() {
+        super.draw();
+        this.image.onload = ()=>{
+            this.context.drawImage(this.image,this.x, this.y);
+        }
         
     }
 }
+
+class TextField extends DrawObject {
+    str: string;
+
+    constructor(x: number, y: number, str: string) {
+        super(x, y);
+        this.str = str;
+        
+    }
+    draw() {
+        super.draw();
+      
+        this.context.fillText(this.str, this.x, this.y);
+     
+    }
+}
+
+
+class DrawObjectContainer extends DrawObject {
+    drawList: objectDrawable[] = [];
+
+    addChild(drawable: objectDrawable) {
+        this.drawList.push(drawable);
+    }
+
+    draw() {
+        this.drawList.forEach((value) => {
+            value.draw();
+        });
+    }
+}
+
 window.onload = () => {
     alert(222)
 };
